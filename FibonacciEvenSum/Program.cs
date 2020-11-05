8a78e6c31c0ba493278632bfc6a23bfbe2b9f5cc
Justin-L-Kennedy <justin.l.kennedy@outlook.com>
2020-11-05 00:15:08 -0600

    Fibonacci Even Sum < 4000000

--- FibonacciEvenSum/Program.cs
+++ FibonacciEvenSum/Program.cs
@@ -10,6 +10,24 @@ namespace FibonacciEvenSum
     {
         static void Main(string[] args)
         {
+            int nbr1 = 0;
+            int nbr2 = 1;
+            int evenSum = 0;
+            for (int i = 1; i < 40; i++)
+            {
+                int sequence = nbr1 + nbr2;
+                if (sequence < 4000000) //sequence value under 4 million
+                {
+                    Console.WriteLine(sequence);
+                    nbr1 = nbr2;
+                    nbr2 = sequence;
+                    if (sequence % 2 == 0) //find only even numbers in sequence
+                    {
+                        evenSum += sequence;
+                    }
+                }
+            }
+            Console.WriteLine(evenSum);
         }
     }
 }
